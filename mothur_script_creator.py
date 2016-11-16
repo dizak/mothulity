c30dbfdcb7f45250c407c0469e1f4730074c2fa6
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2016-11-16 11:06:42 +0100

    ENH@Bunch of var in script and template

--- mothur_script_creator.py
+++ mothur_script_creator.py
@@ -10,8 +10,38 @@ def load_template(template_file):
     return template
 
 
-def render_template(template_loaded):
-    template_vars = {}
+def render_template(template_loaded,
+                    job_name,
+                    partition = "long",
+                    nodes = 1,
+                    ntasks_per_node = 6,
+                    mem_per_cpu = 24,
+                    node_list = None,
+                    processors = 12,
+                    max_ambig = 0,
+                    max_homop = 8,
+                    min_length = 400,
+                    max_length = 500,
+                    min_overlap = 25,
+                    screen_criteria,
+                    precluster_diffs = 4,
+                    chimera_dereplicate = "T"):
+    mem_per_cpu = "{0}G".format(mem_per_cpu)
+    template_vars = {"job_name": job_name,
+                     "partition": partition,
+                     "nodes": nodes,
+                     "ntasks_per_node": ntasks_per_node,
+                     "mem_per_cpu": mem_per_cpu,
+                     "node_list": node_list,
+                     "processors": processors,
+                     "max_ambig": max_ambig,
+                     "max_homop": max_homop,
+                     "min_length": min_length,
+                     "max_length": max_length,
+                     "min_overlap": min_overlap,
+                     "screen_criteria": screen_criteria,
+                     "precluster_diffs": precluster_diffs,
+                     "chimera_dereplicate": chimera_dereplicate}
     template_rendered = template_loaded.render(template_vars)
 
 
