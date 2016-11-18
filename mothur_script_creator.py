3a0f9a420d09d8b71543871be18b8468ccdd3544
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2016-11-18 16:01:09 +0100

    ENH:template is now hard-coded so that no link can be lost. There is still an opt to load custom template

--- mothur_script_creator.py
+++ mothur_script_creator.py
@@ -4,6 +4,11 @@ import jinja2 as jj2
 import argparse
 
 
+def load_template_str(template_str):
+    template = jj2.Environment().from_string(template_str)
+    return template
+
+
 def load_template(template_file):
     template_Loader = jj2.FileSystemLoader(searchpath = ".")
     template_Env = jj2.Environment(loader = template_Loader)
@@ -60,6 +65,8 @@ def save_template(out_file_name,
 
 
 def main():
+    templ_str = """#!/bin/bash\n\n#SBATCH --job-name="{{job_name}}"\n#SBATCH --partition={{partition}}\n#SBATCH --nodes={{nodes}}\n#SBATCH --ntasks-per-node={{ntasks_per_node}}\n#SBATCH --mem-per-cpu={{mem_per_cpu}}\n{%if node_list != None%}#SBATCH --nodelist={{node_list}}{%endif%}\n\nmothur '#set.current(processors={{processors}}); make.contigs(file={{job_name}}.files); summary.seqs(fasta=current); screen.seqs(fasta=current, contigsreport={{job_name}}.contigs.report, group=current, maxambig={{max_ambig}}, maxhomop={{max_homop}}, minlength={{min_length}}, maxlength={{max_length}}, minoverlap={{min_overlap}}); summary.seqs(fasta=current); unique.seqs(fasta=current); count.seqs(name=current, group=current); summary.seqs(fasta=current, count=current); align.seqs(fasta=current, reference=/home/dizak/db/Silva.nr_v119/silva.nr_v119.align); summary.seqs(fasta=current, count=current); screen.seqs(fasta=current, count=current, summary=current,  optimize=start-end, criteria={{screen_criteria}}); summary.seqs(fasta=current, count=current); filter.seqs(fasta=current, vertical=T, trump=.); unique.seqs(fasta=current, count=current); summary.seqs(fasta=current, count=current); pre.cluster(fasta=current, count=current, diffs={{precluster_diffs}}); chimera.uchime(fasta=current, count=current, dereplicate={{chimera_dereplicate}}); remove.seqs(fasta=current, accnos=current); summary.seqs(fasta=current, count=current); classify.seqs(fasta=current, count=current, reference=/home/dizak/db/Silva.nr_v119/silva.nr_v119.align, taxonomy=/home/dizak/db/Silva.nr_v119/silva.nr_v119.tax, cutoff={{classify_seqs_cutoff}}); remove.lineage(fasta=current, count=current, taxonomy=current, taxon=Chloroplast-Mitochondria-Eukaryota-unknown-Unknown);{%if mock == True%} remove.groups(fasta=current, count=current, taxonomy=current, groups=Mock); cluster.split(fasta=current, count=current, taxonomy=current, splitmethod=classify, taxlevel=4, cutoff={{cluster_cutoff}}); make.shared(list=current, count=current, label=0.03); classify.otu(list=current, count=current, taxonomy=current, label=0.03); count.groups(shared=current); phylotype(taxonomy=current); make.shared(list=current, count=current, label=1); classify.otu(list=current, count=current, taxonomy=current, label=1); system(cp zury_V3_V4.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.fasta mock.fasta); system(cp zury_V3_V4.trim.contigs.good.unique.good.filter.unique.precluster.denovo.uchime.pick.pick.count_table mock.count_table); get.groups(fasta=mock.fasta, count=mock.count_table, groups=Mock); seq.error(fasta=current, count=current, reference=HMP_MOCK.v35.fasta, aligned=F); dist.seqs(fasta=current, cutoff=0.20); cluster(column=current, count=current); make.shared(list=current, count=current, label=0.03); rarefaction.single(shared=current){%else%}cluster.split(fasta=current, count=current, taxonomy=current, splitmethod=classify, taxlevel=4, cutoff={{cluster_cutoff}}); make.shared(list=current, count=current, label=0.03); classify.otu(list=current, count=current, taxonomy=current, label=0.03); count.groups(shared=current); phylotype(taxonomy=current); make.shared(list=current, count=current, label=1); classify.otu(list=current, count=current, taxonomy=current, label=1){%endif%}'"""
+
     parser = argparse.ArgumentParser(description = "creates headnode-suitable\
                                                     mothur script",
                                      version = "tests")
@@ -99,7 +106,7 @@ def main():
     if args.template_file_name != None:
         loaded_template = load_template(args.template_file_name)
     else:
-        loaded_template = load_template("mothur_script_template")
+        loaded_template = load_template_str(templ_str)
     rendered_template = render_template(loaded_template,
                                         args.job_name,
                                         args.mock)
