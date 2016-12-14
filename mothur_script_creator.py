44b1c712207677baa85f97f1f046d88a894dce57
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2016-12-14 16:37:53 +0100

    FIX@ArgParse:Typo

--- mothur_script_creator.py
+++ mothur_script_creator.py
@@ -218,17 +218,16 @@ def main():
                         help = "bootstrap value for taxonomic assignment.\
                                 classify.seqs param. Default <80>.")
     mothur.add_argument("--align-database",
+                        required = True,
                         action = "store",
                         dest = "align_database",
                         metavar = "",
-                        required = True,
                         help = "path/to/align-database. Used by align.seqs\
                                 command as <reference> argument. Required.")
     mothur.add_argument("--taxonomy-database",
                         action = "store",
                         dest = "taxonomy_database",
                         metavar = "",
-                        required = True,
                         help = "path/to/taxonomy-database. Used by\
                                 classify.seqs as <taxonomy> argument.\
                                 Required")
