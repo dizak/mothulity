cfd9b00a8279f9bbbdd87f60fb49c978a06fa595
dizak <dariusz.izak@ibb.waw.pl>
2017-05-23 16:54:25 +0200

    UPD:Moved module-wide useful stuff to utilities

--- mothulity.py
+++ mothulity.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 
+from utilities import get_dir_path, dict2cache, cache2dict
 import time
 import jinja2 as jj2
 import argparse
@@ -16,51 +17,6 @@ import pandas as pd
 
 __author__ = "Dariusz Izak IBB PAS"
 __version = "0.9.7"
-
-
-def get_dir_path(file_name=""):
-    """
-    Find out what is the script system path and return its location. Optionally
-    put desired file name at the end of the path. Facilitates access to files
-    stored in the same directory as executed script. Requires the executed
-    script being added to the system path
-    Parameters
-    --------
-    file_name: str, default <"">
-        File name to put at the end of the path. Use empty string if want just
-        the directory.
-    Returns
-    --------
-    str
-        System path of the executable.
-    Examples
-    -------
-    >>> get_dir_path()
-    '/home/user/program/bin/'
-    >>> get_dir_path("foo")
-    '/home/user/program/bin/foo'
-    """
-    prog_path = sys.argv[0].replace(sys.argv[0].split("/")[-1],
-                                    file_name)
-    return prog_path
-
-
-def dict2cache(cache_name,
-               input_dict):
-    try:
-        cache = shelve.open(cache_name)
-        for i in input_dict:
-            cache[i] = input_dict[i]
-    finally:
-        cache.close()
-
-
-def cache2dict(cache_name):
-    try:
-        cache = shelve.open(cache_name)
-        return dict(cache)
-    finally:
-        cache.close()
 
 
 def load_template_str(template_str):
