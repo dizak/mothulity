15f406ee1adec34e7e75d149e7ddecf4c5452d5e
dizak <dariusz.izak@ibb.waw.pl>
2017-07-07 09:19:45 +0200

    UPD:Vars declarations, finding conda, env creation if anaconda-per-user

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+
+# Define variables
+## Where is ~/.bashrc
+BASH_RC=${HOME}/.bashrc
+echo $BASH_RC
+## Where is conda
+CONDA_PATH=$(which conda)
+echo $CONDA_PATH
+## Where is mothulity
+MOTHULITY_PATH=$(dirname $0)
+echo $MOTHULITY_PATH
+# Check for Anaconda
+if [ ${#CONDA_PATH} -eq 0 ]; then
+  echo 'Please install Anaconda first.';
+  exit;
+else echo "Found Anaconda in: ${CONDA_PATH}";
+fi
+# Add mothulity to PATH
+# Create mothulity env from mothulity.yaml
+if [ "$CONDA_PATH"==*"$HOME"* ]; then
+  echo "conda env create --file /path/to/mothulity.yaml"
+else echo "mkdir "${MOTHULITY_PATH}/env"";
+  echo "${HOME}/${MOTHULITY_PATH}"
+fi
