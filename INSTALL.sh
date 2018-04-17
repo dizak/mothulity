3b25e7c6bcaf3f942d5482a9a7ac28d2190576c1
dizak <dariusz.izak@ibb.waw.pl>
2018-04-17 14:37:01 +0200

    ENH:Downloading and setting default databases during install

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Define variables
+## Good-bye message
+BYE="Thanks for installing mothulity. Hope it will save you as much work as possible!\nReport bugs and other issues at https://github.com/dizak/mothulity/issues"
 ## Where is ~/.bashrc
 BASH_RC=${HOME}/.bashrc
 ## Where is conda
@@ -39,21 +41,59 @@ cd $MOTHULITY_PATH
 # Run unittests
 python -m unittest -v tests.tests;
 # Prompt for databases download
-echo 'Mothulity needs databases to work its magic. Would you like to download them now? [yes|no].
-You can always download the databases later with mothulity_dbaser.py and set the default database path with
-mothulity.py --set-align-database-path and
-mothulity.py --set-taxonomy-database-path'
-read DB_GEN_ANSWER;
-if [ $DB_GEN_ANSWER -eq 'yes' ]; then
-  echo 'Which database would you like to download?
-[UNITE ITS 02|UNITE ITS s 02|Silva v102|Silva v119|Silva v123]'
-  read DB_TYPE_ANSWER;
-  echo 'Where would you like to download it? It will be set as default database mothulity path.'
-  read DB_PATH_ANSWER;
-  case $DB_TYPE_ANSWER in
-    "UNITE ITS 02") mothulity_dbaser.py /path/to/databases $DB_PATH_ANSWER --unite-ITS-02; mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/Unite_ITS_02/UNITEv6_sh_99.fasta" --set-taxonomy-database-path "${DB_PATH_ANSWER}/UNITEv6_sh_99.tax"
-    "UNITE ITS s 02") mothulity_dbaser.py /path/to/databases $DB_PATH_ANSWER --unite-ITS-s-02; mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/Unite_ITS_s_02/UNITEv6_sh_97_s.fasta" --set-taxonomy-database-path "${DB_PATH_ANSWER}/UNITEv6_sh_97_s.tax"
-    "Silva v102") mothulity_dbaser.py /path/to/databases $DB_PATH_ANSWER --silva-102; mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/" --set-taxonomy-database-path "${DB_PATH_ANSWER}/"
-    "Silva v119") mothulity_dbaser.py /path/to/databases $DB_PATH_ANSWER --silva-119; mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/silva.nr_v119.align" --set-taxonomy-database-path "${DB_PATH_ANSWER}/silva.nr_v119.tax"
-    "Silva v123") mothulity_dbaser.py /path/to/databases $DB_PATH_ANSWER --silva-123; mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/" --set-taxonomy-database-path "${DB_PATH_ANSWER}/"
-fi
+echo "Mothulity needs databases to work its magic. Would you like to download them now?\n[yes|no]\n"
+while read DB_GEN_ANSWER
+do
+  if [ "$DB_GEN_ANSWER" = 'yes' ]; then
+    echo 'Where would you like to download it?';
+    while read DB_PATH_ANSWER;
+    do
+      if [ -e "$DB_PATH_ANSWER" ]; then
+        echo "\nIt will be set as default database path."
+        break
+      else
+        echo 'Cannot find path. Try again.'
+      fi
+    done
+    echo "Which database would you like to download?\n[1] UNITE ITS 02\n[2] UNITE ITS s 02\n[3] Silva v102\n[4] Silva v119\n[5] Silva v123";
+    while read DB_TYPE_ANSWER;
+    do
+      case "$DB_TYPE_ANSWER" in
+        1)
+        mothulity_dbaser.py $DB_PATH_ANSWER --unite-ITS-02 &&
+        mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/Unite_ITS_02/UNITEv6_sh_99.fasta" --set-taxonomy-database-path "${DB_PATH_ANSWER}/UNITEv6_sh_99.tax"
+        break
+        ;;
+        2)
+        mothulity_dbaser.py $DB_PATH_ANSWER --unite-ITS-s-02 &&
+        mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/Unite_ITS_s_02/UNITEv6_sh_97_s.fasta" --set-taxonomy-database-path "${DB_PATH_ANSWER}/UNITEv6_sh_97_s.tax"
+        break
+        ;;
+        3)
+        mothulity_dbaser.py $DB_PATH_ANSWER --silva-102
+        break
+        ;;
+        4)
+        mothulity_dbaser.py $DB_PATH_ANSWER --silva-119 &&
+        mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/silva.nr_v119.align" --set-taxonomy-database-path "${DB_PATH_ANSWER}/silva.nr_v119.tax"
+        break
+        ;;
+        5)
+        mothulity_dbaser.py $DB_PATH_ANSWER --silva-123 &&
+        mothulity.py . --set-align-database-path "${DB_PATH_ANSWER}/" --set-taxonomy-database-path "${DB_PATH_ANSWER}/"
+        break
+        ;;
+        *)
+        echo 'No such database.'
+        ;;
+      esac
+    done
+    echo $BYE
+    exit
+  elif [ "$DB_GEN_ANSWER"  = 'no' ]; then
+    echo $BYE
+    exit
+  else
+    echo 'Please answer yes or no.'
+  fi
+done
